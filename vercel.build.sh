1a79744ee71850f8966e1e0dd1bfc200a38addb5
CHUJUNGE <chujung@student.unimelb.edu.au>
2025-02-05 14:13:16 +1100

    Update Vercel configuration for custom Flutter build

--- vercel.build.sh
+++ vercel.build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# 下载 Flutter SDK
-git clone https://github.com/flutter/flutter.git
+# 下载并解压 Flutter SDK
+FLUTTER_VERSION="3.16.5"
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+
+# 设置环境变量
 export PATH="$PATH:$(pwd)/flutter/bin"
 
-# 安装 Flutter 依赖
-flutter precache
+# 禁用 Flutter 分析
+export FLUTTER_CLI_ANALYTICS=false
+
+# 安装依赖
+flutter config --no-analytics
 flutter doctor
+flutter pub get
 
 # 构建 Web 应用
-flutter pub get
 flutter build web --release
